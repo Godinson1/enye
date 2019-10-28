ae1b7f795f7649e6e3300fa7e86d054212e7025c
JOSEPH GODWIN <josephgodwin274@yahoo.com>
2019-10-28 09:33:13 +0100

    Update userAction.js

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -1,6 +1,12 @@
-import {  UPDATED } from './type';
+import {  UPDATED, NEW_UPDATED } from './type';
 
 export const update = data => ({
     type: UPDATED,
+    payload: {data}
+});
+
+
+export const update_store = data => ({
+    type: NEW_UPDATED,
     payload: {data}
 });
